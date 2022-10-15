82fdf6136b60e7b92d21ce8c58fd988dcfd866f0
Noah Nguyen <112711706+NoahNg422@users.noreply.github.com>
2022-10-15 12:00:31 -0500

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -9,40 +9,40 @@ def main():
     cpu = options[random.randint(0, 2)]
     if player1 == 'rock':
       if cpu == 'rock':
-        print(f'\nyour choice: {player1}; Fly\'s choice: {cpu}\n')
+        print(f'\nyour choice: {player1}; CPU\'s choice: {cpu}\n')
         print('Tied, going again\n')
       elif cpu == 'paper':
         wins = 1
-        print(f'\nyour choice: {player1}; Fly\'s choice: {cpu}\n')
-        print('Fly Dragon won! (you lost)')
+        print(f'\nyour choice: {player1}; CPU\'s choice: {cpu}\n')
+        print('CPU won! (you lost)')
       elif cpu == 'scissors':
         wins = 1
-        print(f'\nyour choice: {player1}; Fly\'s choice: {cpu}\n')
-        print('You won! You beat Fly Dragon!')
+        print(f'\nyour choice: {player1}; CPU\'s choice: {cpu}\n')
+        print('You won! You beat CPU!')
     elif player1 == 'paper':
       if cpu == 'paper':
-        print(f'\nyour choice: {player1}; Fly\'s choice: {cpu}\n')
+        print(f'\nyour choice: {player1}; CPU\'s choice: {cpu}\n')
         print('Tied, going again\n')
       elif cpu == 'scissors':
         wins = 1
-        print(f'\nyour choice: {player1}; Fly\'s choice: {cpu}\n')
-        print('Fly Dragon won! (you lost)')
+        print(f'\nyour choice: {player1}; CPU\'s choice: {cpu}\n')
+        print('CPU won! (you lost)')
       elif cpu == 'rock':
         wins = 1
-        print(f'\nyour choice: {player1}; Fly\'s choice: {cpu}\n')
-        print('You won! You beat Fly Dragon!')
+        print(f'\nyour choice: {player1}; CPU\'s choice: {cpu}\n')
+        print('You won! You beat CPU!')
     elif player1 == 'scissors':
       if cpu == 'scissors':
-        print(f'\nyour choice: {player1}; Fly\'s choice: {cpu}\n')
+        print(f'\nyour choice: {player1}; CPU\'s choice: {cpu}\n')
         print('Tied, going again\n')
       elif cpu == 'rock':
         wins = 1
-        print(f'\nyour choice: {player1}; Fly\'s choice: {cpu}\n')
-        print('Fly Dragon won! (you lost)')
+        print(f'\nyour choice: {player1}; CPU\'s choice: {cpu}\n')
+        print('CPU won! (you lost)')
       elif cpu == 'paper':
         wins = 1
-        print(f'\nyour choice: {player1}; Fly\'s choice: {cpu}\n')
-        print('You won! You beat Fly Dragon!')
+        print(f'\nyour choice: {player1}; CPU\'s choice: {cpu}\n')
+        print('You won! You beat CPU!')
     else:
       print('Invalid option!')
 
